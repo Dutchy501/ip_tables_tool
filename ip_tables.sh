6f88149bd6381eba5d29af04f3b8c11fc1171e0a
Phillip Holland <94298255+Dutchy501@users.noreply.github.com>
2026-01-06 05:12:39 -0600

    Updated ip_tables.sh

--- ip_tables.sh
+++ ip_tables.sh
@@ -6,10 +6,15 @@ if [[ "$EUID" -ne 0 ]]; then
   exit 1
 fi
 
-dnf install -y iptables-services
-systemctl enable iptables && systemctl start iptables
-iptables -I INPUT 1 -p tcp -s <ip address> --dport <port> -m conntrack --ctstate NEW -j ACCEPT
-iptables -I INPUT 2 -p tcp --dport <port> -j REJECT --reject-with tcp-reset
+yum install -y iptables-services
+systemctl enable --now iptables
+iptables -F
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -s <ip address> --dport <port> -j ACCEPT
+#Add this to drop all other connections on the same port from different ipaddresses
+iptables -A INPUT -p tcp --dport <port> -j DROP
 service iptables save
 
 iptables -L INPUT -n --line-numbers
